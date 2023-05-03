a1d0ddd43e24d75166ed8b1d3b542bab982925da
Naharun <naharunsraboni@gmail.com>
2023-05-04 02:55:07 +0600

    hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCBQldNQQq3leqhnC92gAspguPvxnIrBko",
-  authDomain: "chef-master-with-client.firebaseapp.com",
-  projectId: "chef-master-with-client",
-  storageBucket: "chef-master-with-client.appspot.com",
-  messagingSenderId: "582516774862",
-  appId: "1:582516774862:web:a6e0b7047f4e17403ebd9f"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
